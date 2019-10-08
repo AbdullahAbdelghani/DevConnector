b8f4f7bac2b7b74ce3f9497de982cf3301ecf88e
AbdullahMuhammad97 <abdullah.cs226@gmail.com>
2019-10-08 14:53:54 +0200

    the back-end
    i finished all the back end offline before i upload the project to github

---
+++ routes/api/users.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const User = require("../../models/User");
+//@route   POST api/users
+//@desc    register user
+//@acess   Public
+router.post(
+  "/",
+  [
+    check("name", "name is required")
+      .not()
+      .isEmpty(),
+    check("email", "please include a valid email").isEmail(),
+    check(
+      "password",
+      "please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exist" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        id: user.id
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtsecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.send({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
