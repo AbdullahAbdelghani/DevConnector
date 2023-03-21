b64a812ad6a933b2c08193874351f51f64f186f0
unknown <abdullah.cs226@gmail.com>
2023-03-21 14:09:53 +0200

    updating the packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Navbar from "./component/layout/Navbar";
 import Landing from "./component/layout/Landing";
 import Login from "./component/auth/Login";
 import Register from "./component/auth/Register";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import {Provider} from "react-redux";
+import store from "./store";
+
 const App = () => (
+  <Provider store={store}>
   <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
+      <header style={{marginBottom:'60px'}}><Navbar/></header>
+      <Routes >
+        <Route exact path="/" element={<Landing/>} />
+        <Route exact path="/register" element={<Register/>} />
+        <Route exact path="/login" element={<Login/>} />
+      </Routes>
   </Router>
+  </Provider>
 );
 
 export default App;
