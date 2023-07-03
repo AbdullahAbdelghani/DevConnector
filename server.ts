f35a06ad5577b56fb7ed46765f73af3d32c5a367
AbdullahMuhammad97 <abdullah.cs226@gmail.com>
2023-07-03 16:00:05 +0300

    production adjusements

--- server.ts
+++ server.ts
@@ -1,24 +1,31 @@
 import { Request, Response } from "express";
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
+app.use(express.json());
 
 //connect DataBase
 connectDB();
-
-//init midleware
-app.use(express.json({ extended: false }));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Heey Gentlemen");
-});
 
 //Define Routes
 app.use("/api/users", require("./routes/api/users.ts"));
 app.use("/api/auth", require("./routes/api/auth.ts"));
 app.use("/api/profile", require("./routes/api/profile.ts"));
 app.use("/api/posts", require("./routes/api/posts.ts"));
+
+//init midleware
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+app.get("*", function (_: Request, res: Response) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
 const port = process.env.PORT || 5000;
 
